a78e071dd5a03c93944b476bc214e7f17db56dc7
Amandeep <148987557+AmanTechWiz@users.noreply.github.com>
2024-11-10 12:03:53 +0530

    Time: 14 ms (35.22%), Space: 25.1 MB (23.6%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     int smallestDivisor(vector<int>& nums, int threshold) {
-       int n = nums.size();
-        if (n > threshold) return -1;
-        
-        // Initialize binary search bounds
-        int low = 1, high = *max_element(nums.begin(), nums.end());
+          int n = nums.size();
+       if(n>threshold) return -1; //edge case
 
-        // Apply binary search
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (sumByD(nums, mid) <= threshold) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;  
+       long long low = 1;
+       long long high = *max_element(nums.begin(),nums.end());
+
+      while(low<=high){
+        long long mid = (low + high)/ 2;
+
+        if(sumBy(nums,mid,n) <= threshold){
+          high = mid - 1;
+        }else{low = mid+1;}
+      }
+      return low;
     }
-
 private:
-    /* Helper function to find the
-    summation of division values*/
-    int sumByD(vector<int>& nums, int threshold) {
-        // Size of array
-        int n = nums.size(); 
+    long long sumBy(vector<int>& nums, int mid , int n) {
         
         // Find the summation of division values
-        int sum = 0;
+        long long sum = 0;
         for (int i = 0; i < n; i++) {
-            sum += ceil((double)(nums[i]) / (double)(threshold));
+            sum += ceil((double)(nums[i]) / (double)(mid));
         }
         return sum;
-    }
-
+    }  
 };
