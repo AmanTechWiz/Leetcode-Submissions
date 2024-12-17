fdb0b51a69c326999e506fbfff67330d1008bdc5
Amandeep <148987557+AmanTechWiz@users.noreply.github.com>
2024-12-17 23:21:42 +0530

    Time: 4 ms (52.53%), Space: 64.7 MB (90.43%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
-    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
-        int cnt_max = 0; 
-        int index = 0;   
-        int n = mat.size(); 
-        int m = mat[0].size(); 
+        vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+    int row = mat.size();
+    int col = mat[0].size();
 
-        for (int i = 0; i < n; i++) {
-            sort(mat[i].begin(), mat[i].end());
-            int pos = lowerbound(mat[i], m, 1); 
-            int cnt = m - pos; 
+    int cntmax=0;
+   int maxRow=0;
 
-            if (cnt > cnt_max) { 
-                cnt_max = cnt;
-                index = i;
-            }
-        }
-        return {index, cnt_max}; 
+    for(int i=0; i<row; i++){
+      int cnt1= 0;
+      for(int j=0; j<col; j++){
+        cnt1+= mat[i][j];
+
+      }  
+      if(cnt1 > cntmax){
+        cntmax = cnt1;
+        maxRow = i;
+      }
+      
     }
-
-    int lowerbound(vector<int>& arr, int m, int x) {
-        int low = 0, high = m - 1;
-        int ans = m;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (arr[mid] == x) {
-                ans = mid;    
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return ans;
+   return {maxRow, cntmax};
     }
 };
