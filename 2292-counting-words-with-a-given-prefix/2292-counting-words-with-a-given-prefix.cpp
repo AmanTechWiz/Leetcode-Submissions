7f4569797bd82b1a64e2e530347efde4d97db606
Amandeep <148987557+AmanTechWiz@users.noreply.github.com>
2025-01-09 22:19:15 +0530

    Time: 0 ms (100%), Space: 13.2 MB (75.06%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -3,7 +3,6 @@ public:
     int prefixCount(vector<string>& words, string pref) {
            int count = 0;
     for (const string& word : words) {
-        // Check if the prefix is found at the beginning (position 0)
         if (word.find(pref) == 0) {
             count++;
         }
