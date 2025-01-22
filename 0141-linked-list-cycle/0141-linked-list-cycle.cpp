2ced0e59b8f27d43bf904f74bd0dc321eaa58144
Amandeep <148987557+AmanTechWiz@users.noreply.github.com>
2025-01-23 00:34:35 +0530

    Time: 12 ms (39.74%), Space: 11.8 MB (75.08%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,27 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode* head) {
-        unordered_set<ListNode*> nodeMap;
-        ListNode* temp = head;
-
-        // Traverse the linked list
-        while (temp != nullptr) {
-            // If the node is already in the
-            // Map, there is a loop
-            if (nodeMap.find(temp) != nodeMap.end()) {
+          ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow){
                 return true;
             }
-            // Store the current node
-            // In the map
-            nodeMap.insert(temp);
-            ;
-
-            // Move to the next node
-            temp = temp->next;
         }
-
-        // If the list is successfully traversed
-        // Without a loop, return false
         return false;
     }
 };
