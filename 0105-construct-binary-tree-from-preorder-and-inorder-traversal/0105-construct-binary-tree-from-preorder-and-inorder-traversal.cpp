2c416838333a7d40516f99f2a2886e5f7c46cead
Amandeep <148987557+AmanTechWiz@users.noreply.github.com>
2025-05-22 15:34:17 +0530

    Time: 7 ms (40.32%), Space: 27.6 MB (38.05%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,28 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        deque<int> preorderQueue(preorder.begin(), preorder.end());
-        return build(preorderQueue, inorder);        
-    }
+        unordered_map<int,int>inMap;
 
-private:
-    TreeNode* build(deque<int>& preorder, vector<int>& inorder) {
-        if (!inorder.empty()) {
-            int val = preorder.front();
-            preorder.pop_front();
-            auto it = find(inorder.begin(), inorder.end(), val);
-            int idx = it - inorder.begin();
-
-            TreeNode* root = new TreeNode(val);
-            vector<int> leftInorder(inorder.begin(), inorder.begin() + idx);
-            vector<int> rightInorder(inorder.begin() + idx + 1, inorder.end());
-
-            root->left = build(preorder, leftInorder);
-            root->right = build(preorder, rightInorder);
-
-            return root;
+        for(int i=0; i<inorder.size();i++){
+            inMap[inorder[i]] = i;
         }
 
-        return nullptr;
-    }    
+        TreeNode* root = buildtree(preorder,inorder,0,preorder.size()-1,0,inorder.size()-1,inMap);
+        return root;
+    }
+
+    TreeNode* buildtree(vector<int>& preorder,vector<int>& inorder,int preStart,int preEnd,int inStart,int inEnd,unordered_map<int,int>&inMap){
+        
+        if(preStart>preEnd || inStart>inEnd) return nullptr;
+        
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        int inRoot = inMap[root->val];
+        int numsLeft = inRoot - inStart;
+
+        root->left =  buildtree(preorder,inorder,preStart+1,preStart+numsLeft,inStart,inRoot-1,inMap);
+        root->right = buildtree(preorder,inorder,preStart+numsLeft+1,preEnd,inRoot+1,inEnd,inMap);
+
+        return root;
+    }
 };
